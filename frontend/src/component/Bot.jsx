2eef5c95d2a5890da5f739e423965928ee4292ee
Prince <pk9644167@gmail.com>
2026-06-10 00:55:03 +0530

    Connect frontend to backend

--- frontend/src/component/Bot.jsx
+++ frontend/src/component/Bot.jsx
@@ -3,40 +3,44 @@ import React, { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import { FaUserCircle } from 'react-icons/fa'
 function Bot() {
-    const [messages,setMessages]=useState([])
-    const [input,setInput]=useState("")
-    const [loading,setLoading]=useState(false)
-    const messagesEndRef=useRef(null)
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState("")
+  const [loading, setLoading] = useState(false)
+  const messagesEndRef = useRef(null)
 
-    useEffect(()=>{
-        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-    },[messages])
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages])
 
-    const handleSendMessage = async () => {
-        setLoading(true);
-        if(!input.trim()) return;
-        try {
-           const res=await axios.post("http://localhost:4002/bot/v1/message",{
-                text: input
-            })
-            if(res.status === 200) {
-                setMessages([...messages, { text: res.data.userMessage, sender: 'user' }, { text: res.data.botMessage, sender: 'bot' }]);
-               
-            }
-            console.log(res.data)
-        } catch (error) {
-            console.log("Error sending message:", error);
+  const handleSendMessage = async () => {
+    setLoading(true);
+    if (!input.trim()) return;
+    try {
+      const res = await axios.post(
+        "https://ai-chatbot-ansu.onrender.com/bot/v1/message",
+        {
+          text: input
         }
-         setInput("");
-            setLoading(false);
-    }
+      )
+      if (res.status === 200) {
+        setMessages([...messages, { text: res.data.userMessage, sender: 'user' }, { text: res.data.botMessage, sender: 'bot' }]);
 
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') handleSendMessage()}
-            
+      }
+      console.log(res.data)
+    } catch (error) {
+      console.log("Error sending message:", error);
+    }
+    setInput("");
+    setLoading(false);
+  }
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') handleSendMessage()
+  }
+
   return (
     <div className='flex flex-col min-h-screen bg-[#0d0d0d] text-white'>
-         {/* Navbar & Header */}
+      {/* Navbar & Header */}
       <header className="fixed top-0 left-0 w-full border-b border-gray-800 bg-[#0d0d0d] z-10">
         <div className=" container mx-auto flex justify-between items-center px-6 py-4">
           <h1 className="text-lg font-bold">AI_ChatBot</h1>
@@ -58,11 +62,10 @@ function Bot() {
               {messages.map((msg, idx) => (
                 <div
                   key={idx}
-                  className={`px-4 py-2 rounded-xl max-w-[75%] ${
-                    msg.sender === "user"
-                      ? "bg-blue-600 text-white self-end"
-                      : "bg-gray-800 text-gray-100 self-start"
-                  }`}
+                  className={`px-4 py-2 rounded-xl max-w-[75%] ${msg.sender === "user"
+                    ? "bg-blue-600 text-white self-end"
+                    : "bg-gray-800 text-gray-100 self-start"
+                    }`}
                 >
                   {msg.text}
                 </div>
@@ -73,7 +76,7 @@ function Bot() {
                   Bot is typing...
                 </div>
               )}
-           <div ref={messagesEndRef}/>
+              <div ref={messagesEndRef} />
             </>
           )}
         </div>
@@ -87,12 +90,12 @@ function Bot() {
               type="text"
               className="flex-1 bg-transparent outline-none text-white placeholder-gray-400 px-2"
               placeholder="Ask BotSpoof..."
-             value={input}
-             onChange={(e) => setInput(e.target.value)}
-             onKeyDown={handleKeyPress}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={handleKeyPress}
             />
             <button
-             onClick={handleSendMessage}
+              onClick={handleSendMessage}
               className="bg-green-600 hover:bg-green-700 px-4 py-1 rounded-full text-white font-medium transition-colors"
             >
               Send
